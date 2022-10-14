8e2df5293fde16784a4ecfc4d4c5bf6b57a9a04a
alkasdci <alkas.alkiviadis-freelancer@digitalcareerinstitute.org>
2022-10-14 15:33:59 +0300

    bcrypt ip

--- mentoring/10-14/client/src/components/Register.js
+++ mentoring/10-14/client/src/components/Register.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+import axios from 'axios'
+
 function Register() {
+
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        pass: ''
+    })
+
+    const handleSave = async () => {
+        
+        try {
+            
+            const response = await axios.post('/users/register', data)
+            console.log("🚀 ~ handleSave ~ response", response)
+
+            
+
+        } catch (error) {
+            
+            console.log('Error registering', error.message)
+        }
+
+    }
+
     return ( 
-    <div>
-        Hello register
+    <div className="bg-sky-100 
+    flex 
+    justify-center 
+    items-center 
+    flex-col
+    md:text-green-500
+    w-[100%]
+    
+    h-[100vh]">
+
+        <div className="
+            flex 
+            justify-center 
+            item-center 
+            flex-col
+            
+            w-[400px]
+            gap-[20px]
+        ">
+
+        <input placeholder="type your username" value={data.name} onChange={e => setData({...data, name: e.target.value})}/>
+        <input placeholder="type your email" value={data.email} onChange={e => setData({...data, email: e.target.value})}/>
+        <input placeholder="type your pass" value={data.pass} onChange={e => setData({...data, pass: e.target.value})}/>
+
+        <button onClick={handleSave}>Register</button>
+        </div>
     </div> );
 }
 
--- mentoring/10-14/server/controllers/userController.js
+++ mentoring/10-14/server/controllers/userController.js
@@ -1,7 +1,20 @@
+const User = require('../models/User')
 
 exports.handleRegister = async (req, res) => {
 
-    console.log('hello from register')
+    
+    try {
+        console.log('hello from register')
+    
+        // const result = await User.create(req.body)
 
-    res.send({success: true})
+        const newUser = new User(req.body)
+
+        const result = newUser.save()
+    
+        res.send({success: true})
+        
+    } catch (error) {
+        console.log('error in register', error.message)
+    }
 }
